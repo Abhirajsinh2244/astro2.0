0732e8175b66137c36022610d99521148b089151
dev_abhiraj <abhirajsinh2244@outlook.com>
2026-03-20 13:04:25 +0530

    Delete expense Tracker directory

--- expense Tracker/client/src/lib/api.ts
+++
@@ -1,5 +0,0 @@
-import { hc } from 'hono/client';
-// Ensure this points to your Hono server types
-import type { AppType } from '../../../server/src/index';
-
-export const apiClient = hc<AppType>('http://localhost:3000');
